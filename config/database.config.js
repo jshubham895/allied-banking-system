d63d4739946b2c906486348d4d8be1bd08eb9a9c
Shubham Jain <jshubham895@gmail.com>
2021-06-03 11:37:33 +0530

    Database connected

--- config/database.config.js
+++ config/database.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    url:'mongodb://localhost:27017/basic-banking-system'
+    url:'mongodb+srv://database_admin:database_password@basicbankingsystem.yr3hj.mongodb.net/test?authSource=admin&replicaSet=atlas-2gi27y-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true'
 }
