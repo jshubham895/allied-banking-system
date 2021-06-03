42bbeedaa36e795cfb8dacfa5264202c88e88eb5
Shubham Jain <jshubham895@gmail.com>
2021-06-03 11:55:30 +0530

    Database issues fixed

--- config/database.config.js
+++ config/database.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    url:'mongodb+srv://database_admin:database_password@basicbankingsystem.yr3hj.mongodb.net/test?authSource=admin&replicaSet=atlas-2gi27y-shard-0&readPreference=primary'
+    url:'mongodb+srv://database_admin:database_password@basicbankingsystem.yr3hj.mongodb.net/test'
 }
