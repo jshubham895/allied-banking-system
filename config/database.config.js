ba27ea7d602eeba9da0b7d6a83a14080fdd3cf78
Shubham Jain <jshubham895@gmail.com>
2021-06-03 12:07:43 +0530

    MongoDB Atlas

--- config/database.config.js
+++ config/database.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    url:'mongodb+srv://database_admin:database_password@basicbankingsystem.yr3hj.mongodb.net/test'
+    url:'mongodb+srv://database_admin:<password>@basicbankingsystem.yr3hj.mongodb.net/basic-banking-system?retryWrites=true&w=majority'
 }
